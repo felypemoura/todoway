e74ced911ead02e87a229d9023b22498a96e0c67
lypekjkj <lypezando@gmail.com>
2024-07-25 18:24:58 -0300

    '1.2'

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,8 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "./prisma"
- 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+    trustHost: true,
     pages: {
         error: '/',
         newUser: '/',
